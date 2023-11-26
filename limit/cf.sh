c3a50be05e24e8f52289d8365f82cf4a8a6cc700
rwrtx <152061298+rwrtx@users.noreply.github.com>
2023-11-26 21:36:26 +0700

    Update cf.sh

--- limit/cf.sh
+++ limit/cf.sh
@@ -5,8 +5,8 @@ apt install jq curl -y
 DOMAIN=vpsstore-r32wrt.xyz
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 dns=${sub}.vpsstore-r32wrt.xyz
-CF_ID=emailmu@gmail.com
-CF_KEY=key publikmu
+CF_ID=amandafitrizharifa009@gmail.com
+CF_KEY=6ed124af651064c642b83e93a175fa09f989d
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
