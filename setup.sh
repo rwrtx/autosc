2749b2d8e62dc6cb2f3436ad2e3210ab08f72201
rwrtx <152061298+rwrtx@users.noreply.github.com>
2023-12-05 14:55:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -321,7 +321,7 @@ restart_system(){
 <code>IPVPS      :</code><code>$MYIP</code>
 <code>Domain     :</code><code>$domain</code>
 <code>ISP        :</code><code>$ISP</code>
-<code>OS         :</code><code>${PRETTY_NAME}, ${uname -m}</code>
+<code>OS         :</code><code>$PRETTY_NAME</code>
 <code>Time       :</code><code>$TIMEZONE</code>
 <code>Exp Sc.    :</code><code>$exp</code>
 <code>────────────────────</code>
